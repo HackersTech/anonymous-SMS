f7bacff90c92cb35688cfc6a1489790340e247e9
Hackers Tech <83658615+Hckers-Tech@users.noreply.github.com>
2021-06-12 09:42:31 +0530

    Update k.py

--- k.py
+++ k.py
@@ -22,12 +22,12 @@ bn='''
                     ##
 '''
 print(bn)
-number = input("\033[1;32;40m] enter number with + code etc what ever you hve for india code is +91 !!!\n ->")
+number = input(" enter number with + code etc what ever you hve for india code is +91 !!!\n ->")
 
-Message = input("\n enter Message")
+Message = input(" \n enter Message ")
 k=Message[0:70]
 
-m =input("send message Y/N")
+m =input("send message Y/N ")
 if m== "y" or m== "Y":
   print("\n"+number+"\n"+k+"\n")
   p = requests.post('https://textbelt.com/text', {
